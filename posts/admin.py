d55e06262c21778a8ced286984225d72ba553388
ChekeGT <chekelosos@gmail.com>
2019-03-04 21:55:58 -0600

    Creado el modelo para los posts y su admin.

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,15 @@
+"""Posts app admin configurations module."""
+
+# Django
+
 from django.contrib import admin
 
-# Register your models here.
+# Models
+from .models import Post
+
+
+class PostAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Post, PostAdmin)
--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,24 @@
+"""Post app  models module."""
+
+# Django
+
 from django.db import models
 
-# Create your models here.
+# Models
+from users.models import User
+
+
+class Post(models.Model):
+    """Posts Model."""
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    title = models.CharField(max_length=255)
+    photo = models.ImageField(upload_to='posts/photograph')
+
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        """Return the post title and the creator username."""
+        return f'{self.title} by @{self.user.username}'
