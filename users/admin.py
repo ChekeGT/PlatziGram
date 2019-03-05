d9c9197971f0f68b538ccd7032527952addcce02
ChekeGT <chekelosos@gmail.com>
2019-03-04 21:46:23 -0600

    Agregado el admin para el nuevo modelo de usuario.
    Creamos el admin, para el nuevo modelo de usuario,
    heredando del admin pasado y agregando los campos
    nuevos dentro de los fieldsets.

--- users/admin.py
+++ users/admin.py
@@ -1,7 +1,23 @@
 """Users app Admin module."""
 
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from .models import User
+
+
+class UserAdmin(BaseUserAdmin):
+    fieldsets = BaseUserAdmin.fieldsets + (
+        (
+            'Perfil', {
+                'fields': (
+                    'website',
+                    'bio',
+                    'phone_number',
+                    'picture'
+                )
+            }
+        ),
+    )
+
 
 admin.site.register(User, UserAdmin)
