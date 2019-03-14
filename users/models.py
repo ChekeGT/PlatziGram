ab7ec6a900cb534a73f7ff0e831268d14079c387
ChekeGT <chekelosos@gmail.com>
2019-03-13 21:35:12 -0600

    Creada la funcionalidad de registro en la web.
    Creamos tanto la vista, como el template para
    registrarse en Platzigram, ademas agregamos a
    el archivo de urls el mapeo de esta, y
    modificamos el campo de email de nuestro
    modelo de usuario con el objetivo de hacerlo
    unico.

--- users/models.py
+++ users/models.py
@@ -9,7 +9,7 @@ class User(AbstractUser):
     website = models.URLField(blank=True, max_length=200)
     bio = models.TextField(blank=True)
     phone_number = models.CharField(blank=True, null=True, max_length=20)
-
+    email = models.EmailField(unique=True)
     picture = models.ImageField(blank=True, null=True, upload_to='users/pictures')
 
     def __str__(self):
--- users/views.py
+++ users/views.py
@@ -1,8 +1,24 @@
 """Users app views module."""
 # Django
+
 from django.shortcuts import render, reverse, redirect
 from django.contrib.auth import authenticate, login,logout
 from django.contrib.auth.decorators import login_required
+
+# Exceptions
+
+from django.db.utils import IntegrityError
+
+# Local Apps
+
+from .models import User
+
+# Exceptions
+
+
+class UserCantBeCreated(Exception):
+    """Exception for raise when a user, cant be created"""
+    pass
 
 
 def login_view(request):
@@ -19,8 +35,41 @@ def login_view(request):
     return render(request, 'users/login.html')
 
 
-@login_required()
+@login_required
 def logout_view(request):
     logout(request)
 
     return redirect('login')
+
+
+def signup_view(request):
+    """Signup View."""
+    if request.method == 'POST':
+        errors = []
+
+        username = request.POST['username']
+        password = request.POST['password']
+        password_confirmation = request.POST['password_confirmation']
+        email = request.POST['email']
+
+        try:
+            if password != password_confirmation:
+                errors += 'La contraseña y su confirmacion no coinciden.\n'
+                raise UserCantBeCreated("User cant be created because the password is not the same in both sides.")
+
+            user = User.objects.create_user(username=username, password=password, email=email)
+
+        except (IntegrityError, UserCantBeCreated):
+            if User.objects.filter(username=username):
+                errors += '\nEse nombre de usuario ya esta ocupado.\n'
+
+            if User.objects.filter(email=email):
+                errors += '\nEse correo electronico, ya esta en uso :(.\n'
+
+            return redirect(reverse('signup') + f'?errors={"".join(errors)}')
+
+        user.first_name = request.POST['first_name']
+        user.last_name = request.POST['last_name']
+        user.save()
+        return redirect('login')
+    return render(request, 'users/signup.html')
