e5b8aafe7dc038d577bc3e8ca1a978567e5cc34b
ChekeGT <chekelosos@gmail.com>
2019-03-16 09:41:12 -0600

    Creada la vista para el detalle de un Post.

--- posts/urls.py
+++ posts/urls.py
@@ -4,10 +4,11 @@
 from django.urls import path
 
 # Views
-from posts.views import PostListView, create_post
+from posts.views import PostListView, create_post, PostDetailView
 
 
 urlpatterns = [
     path('', PostListView.as_view(), name='feed'),
     path('posts/new/', create_post, name='create_post'),
+    path('posts/<str:title>/', PostDetailView.as_view(), name='post_detail')
 ]
--- posts/views.py
+++ posts/views.py
@@ -3,7 +3,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 # Forms
 from .forms import CreatePostForm
@@ -13,6 +13,7 @@ from .models import Post
 
 
 class PostListView(LoginRequiredMixin, ListView):
+    """Class based view that manages the feed(List of Posts)."""
     model = Post
     ordering = ('-created',)
     template_name = 'posts/feed.html'
@@ -20,7 +21,7 @@ class PostListView(LoginRequiredMixin, ListView):
 
 
 @login_required
-def create_post(request):
+def create_post(request) -> 'Http Response':
     if request.method == 'POST':
         form = CreatePostForm(request.POST, request.FILES)
         if form.is_valid():
@@ -30,3 +31,12 @@ def create_post(request):
         form = CreatePostForm()
 
     return render(request, 'posts/create.html', {'form': form})
+
+
+class PostDetailView(LoginRequiredMixin, DetailView):
+    """Manages the view of a Post, and shows its details."""
+    model = Post
+    template_name = 'posts/detail.html'
+    context_object_name = 'post'
+    slug_field = 'title'
+    slug_url_kwarg = 'title'
