e9339fa99c57c597d3f970bfba800345ae17ac0b
ChekeGT <chekelosos@gmail.com>
2019-03-24 21:52:56 -0600

    Bug con el campo numero de telefono arreglado.
    Se me habia escapado un null=True y como resultado
    a la hora de editar tu perfil, si no tenias un numero
    de telefono en lugar de aparecer el placeholder te
    aparecia null(por que ese era realmente el dato en la db)
    lo quite y ya no esta mas el problema.

--- users/models.py
+++ users/models.py
@@ -8,7 +8,7 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
     website = models.URLField(blank=True, max_length=200)
     bio = models.TextField(blank=True)
-    phone_number = models.CharField(blank=True, null=True, max_length=20)
+    phone_number = models.CharField(blank=True, max_length=20)
     email = models.EmailField(unique=True)
     picture = models.ImageField(blank=True, null=True, upload_to='users/pictures')
 
